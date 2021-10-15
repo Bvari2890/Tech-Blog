cf3a1b0a6145fcd39c0b56affbee42b13c3d3e93
Bo Vari <bvari2890@gmail.com>
2021-10-15 17:10:46 -0400

    updated api routes for blog, index and user.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-const userRoutes = require('./user-routes');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
+const blogRoutes = require('./blogRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/blogs', blogRoutes);
 router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
 
 module.exports = router;

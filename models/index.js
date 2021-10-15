260fec26b32e0bb57e493fb4953117cdaf0c39d0
Bo Vari <bvari2890@gmail.com>
2021-10-15 17:06:03 -0400

    added blog.js and updated index and user.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const Blog = require('./Blog');
+
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog };
